9e3e575cbc4ee5cb0637aaf11b4429c01bc4c8a6
hksitorus <hk.sitorus@gmail.com>
2025-03-07 09:20:55 +0800

    adjustment
    # Conflicts:
    #	api/server/routes/config.js
    #	api/server/services/ToolService.js
    #	client/public/assets/maskable-icon.png
    #	package-lock.json

--- client/src/hooks/Files/useFileDeletion.ts
+++ client/src/hooks/Files/useFileDeletion.ts
@@ -47,7 +47,6 @@ const useFileDeletion = ({
     [mutateAsync],
   );
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedDelete = useCallback(debounce(executeBatchDelete, 1000), []);
 
   useEffect(() => {
